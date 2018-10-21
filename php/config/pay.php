75b6f33d50d9ff07cb001be9a621972879a9c85c
xnrcms_mac <562909771@qq.com>
2018-10-21 16:19:46 +0800

    bug 修复

--- php/config/pay.php
+++ php/config/pay.php
@@ -36,9 +36,9 @@ return [
             'rsa_private_key'   => 'MIIEpAIBAAKCAQEAv4YpVRiHVFzzAvp3IO6nfwPHCLfEr8fHH1GMTdQnntlgp13OKL33kAXyGhIqGIS3SNuW17c3cTEz0XIIMCaLnOqdv5Bm+c4Zhxxwb7r5hlihTwh/ckOSJoGN1Ajh55nLrhi9mKijgP4N+ZxJGlRKtb43qxp1/aVSwFg++SwU6aJ6KJSoznrkufBVKW4MbpcC+MDcwDWiBH5byOrVNeUp9Fb0y2CXDyZRPiUDUAVto8851QFw0BZ1qvoR0rOMPjfMI5MFkknrJZwUtwPYluxcapaX8eF8mW9jVpoXIA25C46N5jtDhW0IXroVfIXkVY8BlVcP79Kc4PmPh/78DmULawIDAQABAoIBAQC5F4dKXfIrbcjM0BHhGcN11Mi9fBvxZTji44SckrPXqGzoUpeyeCSQY66d04ArQdd/6ffVcZN7KPnTKEkRf67db0AUqhFayfuRv8JJuID9fRonrya73uGXpQzUJeFsWg2lqvNQXWnmd6A54vgjIeMx0SsriN8Oyn82tBHn7NopDtaC/FWmQurlPdJJhWkZFs1Nj8fD6IVPnrdS5ThJ9/voeYF5rdayKOZf36lIx8z/hj5Blhr68o3oWuVBF+OuYf3Smxx/n+5uUDFNCr92nXw0O80Vd1eAai20Ls/S1u2zfpAH7xyqIlxI2L9ezLEY+UOSDNglvMSD1e/nJ0ZLdgYBAoGBAPWrccRgXVbVR6I2w5oy+3t0xhiQr6hntnocDS2S4azVB2sAXlQMD7cYCV1YkzgEUENT4ZGplf4rZxfBxwZPVTO6cYJbeVN4NwBcPNT189okq7HkItdmXHQO1Yhm7vWLkkUoSLaEme/9nq1BAOEkB3Owjq+D5kHTKugTJWz3MzdLAoGBAMeT20yKNnKqhFLRMyZphjoN7zu/UWzKdFz/C1T9lY6EVlIhegfnHqbJ9bbPxB/Mn4ihDegzODnSpAb78yH1dWntDk8UCUjRdFfqIvVELiRdKrD0JkKsncIt0QXXIe4sSLYCeEWRsQUgcrZKzD9560p196Rpo9Fug0PXs+X/UEhhAoGANqGvpQoA98RqL6qaCPp5blTjkKbsSTj3HWSLkazuPq8I/USRtYMRI5hWzMlbw8NBzhcjPG9ICcPBI0lWZxLRUbWOdHy/GE7NfGkGph0j40jwXZjsHpaGzNBXsAOj9DrbhkGVGfGXAgWWedTQy0Bl39ZNhL6CP/Ujv4QyeG7ols0CgYA7OjTVhRrdcp8sWKseViigZ+w8Re2rJHXd905snYjZr4pSe17Uo5EkHEFQTF4+taIOkQUoiLLB7jIBZJnl1QtQMSqS6zZKJuapBHH1aZNr9T4rH3mPRdzXeHNUkWCfZwL5CMialL7874E7ef0dRVg/U7z3TOZxy6Mm6geYVOmFYQKBgQCeADki5KrLBnE//DCeBXDgp8BmO/qFncnpvNYQTlXMvCtSb8kWeB9NCP9OafceUAZaNZ/bhdw+8incUQniFB/MrjTimhmjUOY9NG9mDN2SULr5AWIbu9+ZU1tOW0y3FlWc4LTeP+dsG4J3Zml05n68P05nW9Sa+gpjkEia5WkACg==',
         ],
         'sslpayment'=>[
-            'InstNo'        =>'900000000000008',
-            'MchtNo'        =>'800000000000031',
-            'SignKey'       =>'D9C9F95ED5C25B451DD656F7F8DA1573',
+            'InstNo'        =>'900000000000001',
+            'MchtNo'        =>'800000000000049',
+            'SignKey'       =>'A0E1D9A5EA5484F84B9EC99F78415C48',
             'ReturnURL'     => request()->domain().'/api/Crontab/paySuccess/pay_type/3',
         ],
         'sandpay'=>[
